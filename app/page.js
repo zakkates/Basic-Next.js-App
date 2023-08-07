170dc15efe49b68dd6c00a788f97d88dd37374f7
Zakery Kates <zakkates@simple-edge.com>
2023-08-07 09:45:27 -0500

    Built first React Component: TestContent()

--- app/page.js
+++ app/page.js
@@ -1,41 +1,69 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+const data = [`omg`, `Edgar`, `Stop!`];
+function TestContent() {
+	return (
+		// A comment
+		<div className={styles.test_content_container}>
+			<h1>My Test</h1>
+
+			<p>
+				<strong>Ut enim ad minim veniam</strong>, duis aute irure dolor in
+				reprehenderit in voluptate velit esse
+				<em>
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat.
+				</em>
+			</p>
+
+			<h2>Tester: {data[1]}</h2>
+			<p>
+				Ut enim ad minim veniam, duis aute irure dolor in reprehenderit in
+				voluptate velit esse <em>quis nostrud</em> exercitation ullamco laboris
+				nisi ut aliquip ex ea commodo consequat.
+			</p>
+			<ul
+				style={{
+					margin: `1rem 0`,
+					padding: `0 0 0 2rem`,
+				}}>
+				{data.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<Image
+				className={styles.logo}
+				src="./next.svg"
+				alt="Next.js Logo"
+				width={180}
+				height={37}
+				priority
+				style={{
+					marginBottom: `5rem`,
+				}}
+			/>
+
+			{/* Middle Content w/ Test Content */}
+			<div
+				className={styles.center}
+				style={{ flexFlow: `column` }}>
+				<TestContent />
+			</div>
+
+			{/* Stylelized original content from Vercel */}
 			<div className={styles.description}>
-				<p>
+				<p className={styles.description.p}>
 					You&apos;re checking out the first Next.js app by&nbsp;
 					<code className={styles.code}>Zakery Kates</code>
 				</p>
-				<div>
-					<a
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer">
-						By{" "}
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							className={styles.vercelLogo}
-							width={100}
-							height={24}
-							priority
-						/>
-					</a>
-				</div>
-			</div>
-
-			<div className={styles.center}>
-				<Image
-					className={styles.logo}
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
 			</div>
 
 			<div className={styles.grid}>
